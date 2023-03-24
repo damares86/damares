8a03789fbe1f8fe98b4a54ead71f454f6c542adf
davide <davide.masera@xstream-labs.com>
2023-03-24 15:06:17 +0100

    cookie probably working, need to test more

--- admin/inc/check_cookie.php
+++ admin/inc/check_cookie.php
@@ -3,44 +3,47 @@
 if(isset($_COOKIE['damares-login'])){
     $pieces = explode(",", $_COOKIE['damares-login']);
     $auth->id = $pieces[0];
+    $id = $pieces[0];
     $auth->auth_token = $pieces[1];
     if($auth->checkCookie()){
+                     
+        session_start();
+        $accountroles->account_id = $id; 
         
-        if($email_exists = $auth->emailExists()){
-         
-            session_start();
-            $accountroles->account_id = $auth->id; 
-            
-            
-            $role_id = $accountroles->showAccountRolesId();
-            $role->id = $role_id ;
-            
-            // set session data
-            $_SESSION['loggedin'] = true ;
-            $_SESSION['account_id'] = $auth->id;
-            $_SESSION['role_id'] = $role_id;
-            $_SESSION['rolename'] = $role->showRolenameById();
-            $_SESSION['username'] = $auth->username;
-            $_SESSION['avatar'] = $auth->avatar;
-            
-            // update the login log time
-            $time=date("Y.m.d, G:i:s");
-            $auth->updateLog($time);
-            
-            $plugin->pluginname = "role_redirect" ;
-            
-            if($plugin->itemExists('pluginname') && $plugin->isActive()==1){
-                $stmt = $role->showAllWhere('id',['id']);
-                foreach($stmt as $row){
-                    if($row['redirect']!="none"){
-                        header("Location: ".$row['redirect']."");
-                        exit;
-                    }
+        $account->id = $id ;
+
+        $stmt = $account->showAllWhere('id',['id']);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $role_id = $accountroles->showAccountRolesId();
+        $role->id = $role_id ;
+        
+        // set session data
+        $_SESSION['loggedin'] = true ;
+        $_SESSION['account_id'] = $row['id'];
+        $_SESSION['role_id'] = $role_id;
+        $_SESSION['rolename'] = $role->showRolenameById();
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['avatar'] = $row['avatar'];
+        
+        // update the login log time
+        $time=date("Y.m.d, G:i:s");
+        $auth->updateLog($time);
+        
+        $plugin->pluginname = "role_redirect" ;
+        
+        if($plugin->itemExists('pluginname') && $plugin->isActive()==1){
+            $stmt = $role->showAllWhere('id',['id']);
+            foreach($stmt as $row){
+                if($row['redirect']!="none"){
+                    header("Location: ".$row['redirect']."");
+                    exit;
                 }
             }
-
-            header("Location: ../admin/");
-            exit;
         }
+
+        header("Location: ../admin/");
+        exit;
+      
     }
 }
