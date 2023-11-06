8a53a0aa3c2ff9d9d69d8db20e5fb9c45d5854e1
davide <davide.masera@xstream-labs.com>
2023-11-06 17:30:24 +0100

    wip customers plugin

---
+++ admin/plugins/customers/class/Customer.php
@@ -0,0 +1,19 @@
+<?php 
+
+##############    Damares    ###############
+#                                          #
+#    A backend project by DM WebLab        #
+#   Website: https://www.dmweblab.com      #
+#   GitHub: https://github.com/damares86   #
+#                                          #
+############################################
+
+class Customer extends Common{
+
+    public $table ;
+    public $id ;
+
+
+}
+
+?>
---
+++ admin/plugins/customers/config.php
@@ -0,0 +1,33 @@
+<?php
+
+// plugin information
+
+$pluginname = "customers" ;
+$description = "Create and manage customers" ;
+$link_parent = "customers" ;
+
+// query to create and drop the table
+
+// REMEMBER: add all pages to section tables and also settings pages
+
+$query_create_table = "CREATE TABLE IF NOT EXISTS ".$prefix."customers
+      ( id INT ( 5 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      email VARCHAR(255) NOT NULL,
+      details TEXT DEFAULT NULL,
+      details_opt TEXT DEFAULT NULL)";
+
+$parent_table=[['link'=>'customers',
+                  'label'=>'Customers',
+                  'icon'=>'person-vcard']];
+
+$child_table=[['link'=>'allCustomers',
+                'label'=>'All Customers',
+                'icon'=>'people-file'],
+                ['link'=>'addCustomer',
+                'label'=>'Add a customer',
+                'icon'=>'person-plus-fill']
+               ];
+
+$query_drop_table = "DROP TABLE  ".$prefix."customers";
+
+?>
