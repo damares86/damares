87c84b642b6172d4ef0c13ab1b3df722dd56f7b2
damares86 <davidemasera@gmail.com>
2023-11-24 09:20:04 +0100

    universal calendar wip

---
+++ admin/plugins/calendar/locale/it/calendar_it.php
@@ -0,0 +1,18 @@
+<?php
+
+##############    Damares    ###############
+#                                          #
+#    A backend project by DM WebLab        #
+#   Website: https://www.dmweblab.com      #
+#   GitHub: https://github.com/damares86   #
+#                                          #
+############################################
+
+// label sidebar
+
+$label_customers="Clienti" ;
+$label_all_customers = "Tutti i clienti" ;
+$label_add_a_customer = "Aggiungi un cliente" ;
+
+
+?>
