476130df4681e486a5052fcbbf24ab211f2cfffd
damares86 <davidemasera@gmail.com>
2023-11-24 10:15:54 +0100

    xxx

--- admin/plugins/calendar/config.php
+++ admin/plugins/calendar/config.php
@@ -2,33 +2,25 @@
 
 // plugin information
 
-$pluginname = "customers" ;
-$description = "Create and manage customers" ;
-$link_parent = "customers" ;
+$pluginname = "calendar" ;
+$description = "Manages events and shows in a calendar" ;
+$link_parent = "Calendar" ;
 
 // query to create and drop the table
 
 // REMEMBER: add all pages to section tables and also settings pages
 
-$query_create_table = "CREATE TABLE IF NOT EXISTS ".$prefix."customers
+$query_create_table = "CREATE TABLE IF NOT EXISTS ".$prefix."calendar
       ( id INT ( 5 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      surname VARCHAR(255) NOT NULL,
-      details TEXT DEFAULT NULL,
-      details_opt TEXT DEFAULT NULL)";
+      event_id INT(5) NOT NULL,
+      event_title VARCHAR(255) NOT NULL,
+      event_start_date VARCHAR(255) NOT NULL,
+      event_end_date VARCHAR(255) DEFAULT NULL)";
 
-$parent_table=[['link'=>'customers',
-                  'label'=>'Customers',
-                  'icon'=>'person-vcard']];
+$parent_table=[['link'=>'calendar',
+                  'label'=>'Calendar',
+                  'icon'=>'calendar-week']];
 
-$child_table=[['link'=>'allCustomers',
-                'label'=>'All Customers',
-                'icon'=>'people-fill'],
-                ['link'=>'addCustomer',
-                'label'=>'Add a customer',
-                'icon'=>'person-plus-fill']
-               ];
-
-$query_drop_table = "DROP TABLE  ".$prefix."customers";
+$query_drop_table = "DROP TABLE  ".$prefix."calendar";
 
 ?>
