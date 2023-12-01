1af72a32bd20dd830d8c27f32d090ace93d8a59d
damares86 <davidemasera@gmail.com>
2023-12-01 17:15:46 +0100

    completed updateCalendar method, needs to be tested

--- admin/class/Calendar.php
+++ admin/class/Calendar.php
@@ -15,54 +15,65 @@ class Calendar extends Common{
 
     public function updateCalendar(){
 
-        if(copy('../inc/calendar.json', '../inc/calendar.json.bck'))
+        // query sulla tabella degli eventi e ricreazione del json
+        require_once '../inc/func/calendarSettings.php' ;
+        $this->table = $calendar['table'];
+        $events = $this->showAll('st') ; 
+
+        /* STRUTTURA JSON
+        - id
+        - event_id
+        - event_title
+        - event_start_date
+        - event_end_date
+        - id_event_cat
+        */
+
+        $this->event_title = $calendar['title'];
+
+        $events_arr = [] ;
+        $idx = 0 ;
+        
+        foreach($events as $item)
         {
 
-            // query sulla tabella degli eventi e ricreazione del json
-            require_once '../inc/func/calendarSettings.php' ;
-            $this->table = $calendar['table'];
-            $events = $this->showAll('st') ; 
+            $id_cat = $item['id_calendar_cat'] ?  $item['id_calendar_cat'] : 0 ;
 
-            /* STRUTTURA JSON
-            - id
-            - event_id
-            - event_title
-            - event_start_date
-            - event_end_date
-            - id_event_cat
-            */
+            $ev = array(
+                'id' 	          => $idx, 
+                'event_id'		  => $item['id'],
+                'event_title'	  => $item[''.$this->event_title.''],
+                'event_start_date'=> $item['st'],
+                'event_end_date'  => $item['et'],
+                'id_event_cat'	  => $id_cat
+            );
+            
+            $events_arr[] = $ev ;
 
-            $this->event_title = $calendar['title'];
+            $idx++ ;
 
-            $events_arr = [] ;
-            $idx = 0 ;
-            foreach($events as $item)
-            {
+        }
 
-                $id_cat = $item['id_calendar_cat'] ?  $item['id_calendar_cat'] : 0 ;
 
-                $ev = array(
-                    'id' 	          => $idx, 
-                    'event_id'		  => $item['id'],
-                    'event_title'	  => $item[''.$this->event_title.''],
-                    'event_start_date'=> $item['st'],
-                    'event_end_date'  => $item['et'],
-                    'id_event_cat'	  => $id_cat
-                );
-                
-                $events_arr[] = $ev ;
+        rename("../inc/calendar.json","../inc/calendar.json.bck");
+        $file = "../inc/calendar.json" ;
+        
+        $json=json_encode($events_arr);
+        
+        $resp="";
 
-                $idx++ ;
-
-            }
-
-        } 
+        if(file_put_contents($file, $json, FILE_APPEND)){
+            chmod($file,0777);
+            unlink("../inc/calendar.json.bck");
+            $resp = '&msg=calUp';
+        }
         else 
         {
-            header("Location: ../index.php?p=".$this->page_origin."&err=calNotUp");
-            exit;
+            rename("../inc/calendar.json.bck","../inc/calendar.json");
+            $resp = '&err=calNotUp';
         }
         
+        return $resp ;
 
     }
 
