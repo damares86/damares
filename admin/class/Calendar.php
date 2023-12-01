658351b652cc67d78f81258bfee64ff0e04bf610
damares86 <davidemasera@gmail.com>
2023-12-01 17:50:13 +0100

    added url option in Calendar

--- admin/class/Calendar.php
+++ admin/class/Calendar.php
@@ -15,37 +15,27 @@ class Calendar extends Common{
 
     public function updateCalendar(){
 
-        // query sulla tabella degli eventi e ricreazione del json
         require_once '../inc/func/calendarSettings.php' ;
+
         $this->table = $calendar['table'];
         $events = $this->showAll('st') ; 
 
-        /* STRUTTURA JSON
-        - id
-        - event_id
-        - event_title
-        - event_start_date
-        - event_end_date
-        - id_event_cat
-        */
-
-        $this->event_title = $calendar['title'];
-
         $events_arr = [] ;
-        $idx = 0 ;
         
         foreach($events as $item)
         {
-
+            // check calendar category for color
             $id_cat = $item['id_calendar_cat'] ?  $item['id_calendar_cat'] : 0 ;
+            
+            // check if isset the url in calendarSettings
+            $url = $calendar['url'] ? ",\n'url' => ".$calendar['url'].$item['id']."" : '' ;
 
+            // create the event element
             $ev = array(
-                'id' 	          => $idx, 
-                'event_id'		  => $item['id'],
-                'event_title'	  => $item[''.$this->event_title.''],
-                'event_start_date'=> $item['st'],
-                'event_end_date'  => $item['et'],
-                'id_event_cat'	  => $id_cat
+                'title'	        => $item[''.$this->event_title.''],
+                'start'         => $item['st'],
+                'end'           => $item['et'],
+                'id_event_cat'	=> $id_cat.$url
             );
             
             $events_arr[] = $ev ;
@@ -54,8 +44,9 @@ class Calendar extends Common{
 
         }
 
-
+        // create a backup of the existing calendar.json
         rename("../inc/calendar.json","../inc/calendar.json.bck");
+
         $file = "../inc/calendar.json" ;
         
         $json=json_encode($events_arr);
@@ -63,12 +54,14 @@ class Calendar extends Common{
         $resp="";
 
         if(file_put_contents($file, $json, FILE_APPEND)){
+            // if creates the new file, delete the backup and resp success
             chmod($file,0777);
             unlink("../inc/calendar.json.bck");
             $resp = '&msg=calUp';
         }
         else 
         {
+            // if doesn't creates the new file, change back the name to the backup file and resp error
             rename("../inc/calendar.json.bck","../inc/calendar.json");
             $resp = '&err=calNotUp';
         }
@@ -76,6 +69,8 @@ class Calendar extends Common{
         return $resp ;
 
     }
+
+    // mng calendar?
 
    
 }
--- admin/inc/func/calendarSettings.php
+++ admin/inc/func/calendarSettings.php
@@ -2,5 +2,6 @@
 $calendar = 
   [
      "table" => "EVENT TABLE",
+     "url"   => "url da concatenare all'id alla fine", // opzionale
      "title" => "EVENT TITLE (es. name)"
   ];
