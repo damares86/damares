82d79b381a9f638754bb6f5dc3ca99604d947a9f
damares86 <davidemasera@gmail.com>
2023-12-11 17:03:05 +0100

    wip addCollaboratore

---
+++ admin/inc/func/addCollaboratore.php
@@ -0,0 +1,225 @@
+<div class="page-title">
+  <div class="row">
+    <div class="col-12 col-md-6 order-md-1 order-last">
+      <h3>Aggiungi un collaboratore</h3>
+    </div>
+    <div class="col-12 col-md-6 order-md-2 order-first">
+      <nav
+        aria-label="breadcrumb"
+        class="breadcrumb-header float-start float-lg-end"
+      >
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="index.php"><?=$common_dashboard?></a>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Aggiungi un collaboratore
+          </li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+</div>
+<br>
+
+<section class="section">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                <h4 class="card-title">Aggiungi un nuovo collaboratore</h4>
+                </div>
+                <div class="card-content">
+                <div class="card-body">
+                    <form class="form form-horizontal" action="core/mngCollaboratori.php" method="POST"  enctype="multipart/form-data" data-parsley-validate>
+                    <div class="form-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <label> Nome <span class="text-danger">*</span></label>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="form-group has-icon-left">
+                                        <div class="form-check mandatory">
+                                            <div class="position-relative">
+                                                <input
+                                                type="text"
+                                                class="form-control"
+                                                placeholder="Name"
+                                                id="first-name-icon"
+                                                name="nome"
+                                                data-parsley-required="true"
+
+                                                />
+                                                <div class="form-control-icon">
+                                                <i class="bi bi-person"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                            <div class="col-md-3">
+                                    <label> Cognome <span class="text-danger">*</span></label>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="form-group has-icon-left">
+                                        <div class="form-check mandatory">
+                                            <div class="position-relative">
+                                                <input
+                                                type="text"
+                                                class="form-control"
+                                                placeholder="Name"
+                                                id="first-name-icon"
+                                                name="cognome"
+                                                data-parsley-required="true"
+
+                                                />
+                                                <div class="form-control-icon">
+                                                <i class="bi bi-person"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> 
+                            </div>    
+                        </div>
+                    </div>
+
+                    <div class="row">
+                       
+                        <div class="col-md-2">
+                            <label><?= $common_email ?> <span class="text-danger">*</span></label>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="form-group has-icon-left">
+                                <div class="form-check mandatory">
+                                    <div class="position-relative">
+                                        <input
+                                        type="email"
+                                        class="form-control"
+                                        placeholder="Email"
+                                        id="first-name-icon"
+                                        name="email"
+                                        data-parsley-required="true"
+                                        />
+                                        <div class="form-control-icon">
+                                        <i class="bi bi-envelope"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+                        <?php
+
+                        require "core/collaboratoreDetails.php";
+                        foreach($account_details as $item){
+
+                            $label = "account_add_$item";
+                            $item_label=ucfirst($item);
+
+                        ?>
+                        <div class="col-md-3">
+                            <label><?=$item_label?> <span class="text-danger">*</span></label>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="form-group">
+                                <div class="form-check mandatory">
+                                    <div class="position-relative">
+                                        <?php                                                              
+                                                $type="text";
+                                                if($item=="birth"){
+                                                    $type="date";
+                                                }
+                                            ?>
+                                            <input
+                                            type="<?=$type?>"
+                                            class="form-control"
+                                            placeholder="<?=$item_label?>"
+                                            name="<?=$item?>"
+                                            data-parsley-required="true"
+
+                                            />
+                                            <?php
+                                            
+                                            ?>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php
+
+                        }
+
+                        foreach($account_details_opt as $item){
+
+                            $label = "account_add_$item";
+                            $item_label=ucfirst($item);
+
+                        ?>
+                        <div class="col-md-3">
+                            <label><?=$item?> <?=$account_add_optional?></label>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="form-group">
+                                <div class="position-relative">
+                                    <?php
+                                        $type="text";
+                                        if($item=="birth"){
+                                            $type="date";
+                                        }
+                                    ?>
+                                    <input
+                                    type="<?=$type?>"
+                                    class="form-control"
+                                    placeholder="<?=$item_label?>"
+                                    name="<?=$item?>"
+
+                                    />
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php
+
+                        }
+
+                        ?>
+
+                       
+                        <input type="hidden" name="operation" value="add">
+                        <input type="hidden" name="origin" value="addCollaboratore">
+                      
+                        <div class="col-12 d-flex justify-content-end">
+                            <button
+                            type="submit"
+                            class="btn btn-primary me-1 mb-1"
+                            >
+                            <?=$common_submit?>
+                            </button>
+                            <button
+                            type="reset"
+                            class="btn btn-light-secondary me-1 mb-1"
+                            >
+                            <?=$common_reset?>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+       
+    </div>
+</section>
