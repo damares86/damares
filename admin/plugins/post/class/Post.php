d0f013abdc5b09a7837b064f701c53e4ee95eb0c
damares86 <davidemasera@gmail.com>
2024-01-08 15:44:50 +0100

    plugin post wip

---
+++ admin/plugins/post/class/Post.php
@@ -0,0 +1,22 @@
+<?php 
+
+##############    Damares    ###############
+#                                          #
+#    A backend project by DM WebLab        #
+#   Website: https://www.dmweblab.com      #
+#   GitHub: https://github.com/damares86   #
+#                                          #
+############################################
+
+class Post extends Common{
+
+    public $post_title ;
+    public $post_content ;
+    public $post_author ;
+    public $post_date ;
+
+    
+
+}
+
+?>
