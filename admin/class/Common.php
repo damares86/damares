322fe7cba16f91f8096523838e192dbeb9699953
damares86 <davidemasera@gmail.com>
2024-01-09 17:57:39 +0100

    blog wip

--- admin/class/Common.php
+++ admin/class/Common.php
@@ -397,6 +397,27 @@ public function itemExists($item){
     }
 }
 
+public function countItem($item){
+        
+    // query to check if email exists
+    $query = "SELECT *
+    FROM " .$this->prx. $this->table . "
+    WHERE ".$item." = :".$item."
+    LIMIT 0,1";
+
+    $stmt = $this->conn->prepare( $query );
+
+    $stmt->bindParam(":".$item."", $this->$item);
+
+    // execute the query
+    $stmt->execute();
+
+    // get number of rows
+    $num = $stmt->rowCount();
+
+    return $num ;
+
+}
 
 
 // delete
