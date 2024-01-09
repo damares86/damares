28db1cd228c9bf6260d99844f105c419f4d88d40
damares86 <davidemasera@gmail.com>
2024-01-09 14:33:57 +0100

    grape code format, indent and write working!

--- admin/vendor/composer/installed.php
+++ admin/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'reference' => '69939fd1e1da886f6a0ee208cc310e18d84d2ec4',
+        'name' => '__root__',
+        'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
+            'reference' => '69939fd1e1da886f6a0ee208cc310e18d84d2ec4',
             'dev_requirement' => false,
         ),
         'bdk/backtrace' => array(
@@ -28,10 +28,10 @@
         'bdk/debug' => array(
             'pretty_version' => 'v3.0',
             'version' => '3.0.0.0',
-            'reference' => '4c4678234c7fffca5b4ce1ae7bdd345e27e075d1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../bdk/debug',
             'aliases' => array(),
+            'reference' => '4c4678234c7fffca5b4ce1ae7bdd345e27e075d1',
             'dev_requirement' => false,
         ),
         'bdk/errorhandler' => array(
