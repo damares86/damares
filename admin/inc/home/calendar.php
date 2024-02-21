e9765fb53169713fbb65fab174375c97fd2775d7
davide <davide.masera@xstream-labs.com>
2024-02-21 13:51:22 +0100

    cfa ok

---
+++ admin/inc/home/calendar.php
@@ -0,0 +1,48 @@
+
+<div class="card-header text-center">
+<script src='script/index.global.js'></script>
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prevYear,prev,next,nextYear today',
+        center: 'title',
+        right: 'dayGridMonth,dayGridWeek,dayGridDay'
+      },
+      // initialDate: '2023-01-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: 
+      {
+        url: "inc/calendar.json"
+      }
+        });
+
+    calendar.render();
+  });
+
+</script>
+<style>
+
+  body {
+    margin: 40px 10px;
+    padding: 0;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #calendar {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
+
+</style>
+
+  <div id='calendar'></div>    
+
+</div>
+
