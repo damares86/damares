8ad520fc022d19883984102bb05779340e387795
davide <davide.masera@xstream-labs.com>
2024-02-21 15:53:56 +0100

    rsa plugin wip

---
+++ admin/plugins/rsa/config.php
@@ -0,0 +1,37 @@
+<?php
+
+// plugin information
+
+$pluginname = "rsa" ;
+$description = "RSA Farmaci" ;
+$link_parent = "rsa" ;
+
+// query to create and drop the table
+
+// REMEMBER: add all pages to section tables and also settings pages
+
+$query_create_table = "CREATE TABLE IF NOT EXISTS ".$prefix."pazienti
+      ( id INT ( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      cognome VARCHAR(255) NOT NULL,
+      nome VARCHAR(255) NOT NULL);
+      CREATE TABLE IF NOT EXISTS ".$prefix."farmaci
+      ( id INT ( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      principio VARCHAR(255) NOT NULL,
+      cpr_box INT(10) NOT NULL,
+      magazzino INT(10) DEFAULT 0);
+      CREATE TABLE IF NOT EXISTS ".$prefix."pazientiFarmaci
+      ( id INT ( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      id_pazienti INT(10) NOT NULL,
+      id_farmaci INT(10) NOT NULL,
+      cpr INT(10) NOT NULL);";
+
+$parent_table=[['link'=>'allPazienti',
+                  'label'=>'Pazienti',
+                  'icon'=>'person-vcard'],
+            ['link'=>'allFarmaci',
+                  'label'=>'Elenco farmaci',
+                  'icon'=>'building']];
+
+$query_drop_table = "DROP TABLE  ".$prefix."pazienti, ".$prefix."farmaci, ".$prefix."pazientiFarmaci";
+
+?>
