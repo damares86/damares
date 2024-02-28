95fc4b6f254f21ed43b644035801ef48903d4001
davide <davide.masera@xstream-labs.com>
2024-02-28 14:34:12 +0100

    datatable localization | dynamic locale todo

--- admin/assets/js/pages/datatables.js
+++ admin/assets/js/pages/datatables.js
@@ -1,2 +1,10 @@
 let jquery_datatable = $("#table1").DataTable()
+
+// localization
+// let jquery_datatable = $("#table1").DataTable({
+//     language: {
+//         url: '//cdn.datatables.net/plug-ins/2.0.1/i18n/it-IT.json',
+//     }
+// })
+
 // let jquery_datatable = $("#table1").DataTable({searching: false})
