b6b89adf08bca94b33f47d4773f0097407a4130c
davide <davide.masera@xstream-labs.com>
2024-04-22 11:50:05 +0200

    added dragula in damares

--- admin/inc/func/allLunaPages.php
+++ admin/inc/func/allLunaPages.php
@@ -1,6 +1,6 @@
 <?php
 
-$luna->table = 'luna_parent' ;
+$luna->table = 'luna_parent';
 $stmt = $luna->showAll('id');
 
 ?>
@@ -10,16 +10,13 @@ $stmt = $luna->showAll('id');
       <h3>Pages Management</h3>
     </div>
     <div class="col-12 col-md-6 order-md-2 order-first">
-      <nav
-        aria-label="breadcrumb"
-        class="breadcrumb-header float-start float-lg-end"
-      >
+      <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
-            <a href="index.php"><?=$common_dashboard?></a>
+            <a href="index.php"><?= $common_dashboard ?></a>
           </li>
           <li class="breadcrumb-item active" aria-current="page">
-          Pages Management
+            Pages Management
           </li>
         </ol>
       </nav>
@@ -32,12 +29,49 @@ $stmt = $luna->showAll('id');
 <!-- Basic Tables start -->
 <section class="section">
   <div class="card shadow">
-    <div class="card-header">Manage pages &nbsp; &nbsp; &nbsp; 
-                    <a href="index.php?p=addLunaPage" class="btn icon icon-left btn-success shadow"
-                        ><i data-feather="plus-circle"></i> Add a new parent page</a
-                      ></div>
+    <div class="card-header">Manage pages &nbsp; &nbsp; &nbsp;
+      <a href="index.php?p=addLunaPage" class="btn icon icon-left btn-success shadow"><i data-feather="plus-circle"></i> Add a new parent page</a>
+    </div>
     <div class="card-body">
-     ciao
+      <div class='examples'>
+
+        <div class='parent'>
+          <label for='hy'>There are plenty of events along the lifetime of a drag event. Check out <a href='https://github.com/bevacqua/dragula#drakeon-events'>all of them</a> in the docs!</label>
+          <div class='wrapper'>
+            <div id='left-events' class='container-pages'>
+              <div id="1">As soon as you start dragging an element, a <code>drag</code> event is fired</div>
+              <div id="2">Whenever an element is cloned because <code>copy: true</code>, a <code>cloned</code> event fires</div>
+              <div id="3">The <code>shadow</code> event fires whenever the placeholder showing where an element would be dropped is moved to a different container or position</div>
+              <div id="4">A <code>drop</code> event is fired whenever an element is dropped anywhere other than its origin <em>(where it was initially dragged from)</em></div>
+            </div>
+            <div id='right-events' class='container-pages'>
+              <div>If the element gets removed from the DOM as a result of dropping outside of any containers, a <code>remove</code> event gets fired</div>
+              <div>A <code>cancel</code> event is fired when an element would be dropped onto an invalid target, but retains its original placement instead</div>
+              <div>The <code>over</code> event fires when you drag something over a container, and <code>out</code> fires when you drag it away from the container</div>
+              <div>Lastly, a <code>dragend</code> event is fired whenever a drag operation ends, regardless of whether it ends in a cancellation, removal, or drop</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      <button id="save">Save</button>
+
     </div>
   </div>
 </section>
+
+
+<script src='script/dragula.js'></script>
+<script src='script/example.min.js'></script>
+
+<script>
+        $("#save").click(function() {
+            const data = $('#left-events > div').map(function(index,el) {
+                return el.id
+            }).get()
+
+            console.log(data)
+            })
+    </script>
--- admin/inc/header.php
+++ admin/inc/header.php
@@ -2,7 +2,7 @@
 
 // check if database is configured
 
-if(!is_file('class/Database.php')){
+if (!is_file('class/Database.php')) {
   require "inc/dbdata.php";
   exit;
 }
@@ -11,56 +11,55 @@ session_start();
 
 
 // check if the user is logged in
-require __DIR__."/config.php";
+require __DIR__ . "/config.php";
 
 if (!isset($_SESSION['loggedin']) && !isset($_SESSION['account_id'])) {
-  require "inc/check_cookie.php" ;
+  require "inc/check_cookie.php";
   header('Location: ../login/auth-login.php?err=noLogin');
   exit;
-}else if(isset($_COOKIE['damares-login'])){
+} else if (isset($_COOKIE['damares-login'])) {
   $pieces = explode(",", $_COOKIE['damares-login']);
   $auth->id = $pieces[0];
   $id = $pieces[0];
   $auth->auth_token = $pieces[1];
-  
-  if(!$auth->checkCookie()>0){
+
+  if (!$auth->checkCookie() > 0) {
     header("Location: ../login/auth-login.php?err=noLogin");
     exit;
   }
-  
-  $role->id = $_SESSION['role_id'] ;
-  
-  $plugin->pluginname = "role_redirect" ;
-  
-  if($plugin->itemExists('pluginname') && $plugin->isActive()==1){
-    $stmt = $role->showAllWhere('id',['id']);
-    foreach($stmt as $row){
-      if($row['redirect']!="none"){
-        header("Location: ".$row['redirect']."");
+
+  $role->id = $_SESSION['role_id'];
+
+  $plugin->pluginname = "role_redirect";
+
+  if ($plugin->itemExists('pluginname') && $plugin->isActive() == 1) {
+    $stmt = $role->showAllWhere('id', ['id']);
+    foreach ($stmt as $row) {
+      if ($row['redirect'] != "none") {
+        header("Location: " . $row['redirect'] . "");
         exit;
       }
     }
   }
-  
-  $export = false ;
-  $plugin->pluginname = "export_xlsx" ;
-  
-  if($plugin->itemExists('pluginname') && $plugin->isActive()==1)
-  {
-    $export = true ;
+
+  $export = false;
+  $plugin->pluginname = "export_xlsx";
+
+  if ($plugin->itemExists('pluginname') && $plugin->isActive() == 1) {
+    $export = true;
   }
-  
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?=$common_dashboard?> - damares</title>
 
-    <!--
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?= $common_dashboard ?> - damares</title>
+
+  <!--
     ##############    Damares    ###############
     #                                          #
     #    A backend project by DM WebLab        #
@@ -69,36 +68,24 @@ if (!isset($_SESSION['loggedin']) && !isset($_SESSION['account_id'])) {
     #                                          #
     ############################################
     -->
-    
-    <link rel="stylesheet" href="assets/css/main/app.css" />
-    <link rel="stylesheet" href="assets/css/main/app-dark.css" />
-    <link
-      rel="shortcut icon"
-      href="assets/images/logo/favicon.ico"
-      type="image/x-icon"
-    />
-    <link
-      rel="shortcut icon"
-      href="assets/images/logo/favicon.ico"
-      type="image/png"
-    />
-    <link
-      rel="stylesheet"
-      href="assets/extensions/choices.js/public/assets/styles/choices.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
-      
-    <link rel="stylesheet" href="assets/css/shared/iconly.css" />
-    <?php
 
-    foreach (glob("assets/css/*.css") as $row){
-    ?>
-      <link rel="stylesheet" href="<?=$row?>" />
-    <?php
-    }
-    ?>
-    <script src="assets/extensions/jquery/jquery.min.js"></script>
+  <link rel="stylesheet" href="assets/css/main/app.css" />
+  <link rel="stylesheet" href="assets/css/main/app-dark.css" />
+  <link rel="shortcut icon" href="assets/images/logo/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="assets/images/logo/favicon.ico" type="image/png" />
+  <link rel="stylesheet" href="assets/extensions/choices.js/public/assets/styles/choices.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 
-  </head>
+  <link rel="stylesheet" href="assets/css/shared/iconly.css" />
+
+<?php
+
+  foreach (glob("assets/css/*.css") as $row) {
+  ?>
+    <link rel="stylesheet" href="<?= $row ?>" />
+  <?php
+  }
+  ?>
+  <script src="assets/extensions/jquery/jquery.min.js"></script>
+
+</head>
