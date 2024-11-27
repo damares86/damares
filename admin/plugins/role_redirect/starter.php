f32d38d8fb9ec814a0906f81815d03a73b73fb82
davide <davide.masera@xstream-labs.com>
2024-11-27 18:04:00 +0100

    fix on role_redirect

--- admin/plugins/role_redirect/starter.php
+++ admin/plugins/role_redirect/starter.php
@@ -4,37 +4,74 @@
 
 if ($op == 'add') {
 
-	// operations during the installation
-    $setting->name = 'role_redirect' ;
-    $setting->value = 1 ;
-    if($setting->updateValue()){
-        $plugin->id = $idPlugin ;
-        $plugin->installed = 1 ;
-        $plugin->active = 1 ;
-        $plugin->update(['installed','active'],'id');
-        header("Location: ../index.php?p=allPlugins&msg=pluginAdd");
-        exit;
-    }else{
+    // operations during the installation
+    $setting->name = 'role_redirect';
+    $setting->value = 1;
+    if ($setting->updateValue()) {
+
+        $error = 0;
+        // copy
+        if (copy("$path/locale/en/redirect_en.php", "../locale/en/redirect_en.php")) {
+            chmod("../locale/en/redirect_en.php", 0755);
+        } else {
+            $error++;
+            echo "$dest_file -> $error<br>";
+        }
+
+        if (copy("$path/locale/it/redirect_it.php", "../locale/it/redirect_it.php")) {
+            chmod("../locale/en/redirect_en.php", 0755);
+        } else {
+            $error++;
+            echo "$dest_file -> $error<br>";
+        }
+        $plugin->id = $idPlugin;
+        $plugin->installed = 1;
+        $plugin->active = 1;
+        $plugin->update(['installed', 'active'], 'id');
+
+        $error_msg = "";
+        if ($error == 0) {
+            header("Location: ../index.php?p=allPlugins&msg=pluginAdd");
+            exit;
+        } else {
+            header("Location: ../index.php?p=allPlugins&err=pluginAddErr");
+            exit;
+        }
+    } else {
         header("Location: ../index.php?p=allPlugins&err=pluginAddErr");
         exit;
     }
-	
-}else if($op == 'rm'){
-	
-	// operations during the remove
-    $setting->name = 'role_redirect' ;
-    $setting->value = 0 ;
+} else if ($op == 'rm') {
+
+    // operations during the remove
+    $setting->name = 'role_redirect';
+    $setting->value = 0;
     if ($setting->updateValue()) {
-        $plugin->id = $idPlugin ;
-        $plugin->installed = 0 ;
-        $plugin->active = 0 ;
-        $plugin->update(['installed','active'],'id');
-        header("Location: ../index.php?p=allPlugins&msg=pluginRm");
-        exit;
-      } else {
+
+        $error = 0;
+        // copy
+        if (!unlink("../locale/en/redirect_en.php")) {
+            $error++;
+        }
+        if (!unlink("../locale/it/redirect_it.php")) {
+            $error++;
+        }
+
+        $plugin->id = $idPlugin;
+        $plugin->installed = 0;
+        $plugin->active = 0;
+        $plugin->update(['installed', 'active'], 'id');
+        if ($error == 0) {
+            header("Location: ../index.php?p=allPlugins&msg=pluginRm");
+            exit;
+        } else {
+            header("Location: ../index.php?p=allPlugins&err=pluginRmErr");
+            exit;
+        }
+    } else {
         header("Location: ../index.php?p=allPlugins&err=pluginRmErr");
         exit;
-      }
+    }
 }
 
 require "config.php";
